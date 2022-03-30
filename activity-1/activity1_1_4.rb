9b1cb1c9c7a5abd44efbe329524f1f3439cad840
Zenith Manuel <user.name>
2022-03-30 20:27:25 +0800

    upload answers

---
+++ activity-1/activity1_1_4.rb
@@ -0,0 +1,14 @@
+print "How old are you?"
+name = gets
+print "Welcome #{name.strip}"
+
+name = name.to_i 
+
+print "In 10 years you will be #{name + 10}\n"
+
+print "In 20 years you will be #{name + 20}\n"
+
+print "In 30 years you will be #{name + 30}\n"
+
+print "In 40 years you will be #{name + 40}\n"
+
